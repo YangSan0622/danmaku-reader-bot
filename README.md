1108aa7f64cd7c3fc9451981f9be9f251a00f5ac
DeReaper0622 <dereaper@hotmail.com>
2020-02-24 22:43:56 -0800

    Initial Commit
    It is still a boring bot at this point

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # danmaku-reader-bot
-discord bot that fetchs damaku from bilibili live and reads it in discord channel
+discord bot that fetches Danmaku from Bilibili live and reads it in discord channel
+
+
+You will need to create your own config.json to run this bot, for example
+
+{
+    "prefix":"!",
+    "token":"YOUR_BOT_TOKEN"
+}
+
+and replace YOUR_BOT_TOKEN with the actual token of your bot
---
+++ main.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+const logger = require('winston');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log("Ready");
+});
+
+client.on('message', message => {
+    if (message.content.startsWith(`${config.prefix}`)){
+        console.log(message);
+    } 
+});
+
+client.login(config.token);
+
+
+// Configure logger settings
+// logger.remove(logger.transports.Console);
+// logger.add(new logger.transports.Console, {
+//     colorize: true
+// });
+// logger.level = 'debug';
+
+
+// Initialize Discord Bot
+// var bot = new Discord.Client({
+//    token: config.token,
+//    autorun: true
+// });
+
+
+// bot.on('ready', function (evt) {
+//     logger.info('Connected');
+//     logger.info('Logged in as: ');
+//     logger.info(bot.username + ' - (' + bot.id + ')');
+// });
+
+
+// bot.on('message', function (user, userID, channelID, message, evt) {
+//     // Our bot needs to know if it will execute a command
+//     // It will listen for messages that will start with `!`
+//     if (message.substring(0, 1) == '!') {
+//         var args = message.substring(1).split(' ');
+//         var cmd = args[0];
+
+//         args = args.splice(1);
+//         switch(cmd) {
+//             // !ping
+//             case 'ping':
+//                 bot.sendMessage({
+//                     to: channelID,
+//                     message: 'Pong!'
+//                 });
+//             break;
+//             // Just add any case commands if you want to..
+//          }
+//      }
+// });
